9c2b6715a940374320ca9e4a37b92bd0e9a01fd8
hajiuhajiu <2705322732@qq.com>
2021-06-28 05:19:33 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -436,7 +436,7 @@ function taskUrl(functionId, body = {}) {
   }
 }
 
-function getAuthorShareCode(url = "https://wuzhi03.coding.net/p/dj/d/shareCodes/git/raw/main/jd_updateCash.json") {
+function getAuthorShareCode(url = "https://gitee.com/xr2021/share/raw/master/JD_Cash.json") {
   return new Promise(resolve => {
     $.get({url, headers:{
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
